2254e4a33bf7a12361f70022cde85bd15230e6fb
kiku ishimoto <kikuish@gmail.com>
2020-06-04 01:55:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,11 @@ class Student
 
   def initialize(name, grade, id = nil)
     @id = id
-    @name = name 
+    @name = name
     @grade = grade
-  end 
+  end
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
