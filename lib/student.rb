3e5e6d146bcbcf9f1682f5ff2aac7219064b608f
kiku ishimoto <kikuish@gmail.com>
2020-06-04 01:55:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,11 @@
 class Student
+  attr_accessor :id, :name, :grade
+
+  def initialize(name, grade, id = nil)
+    @id = id
+    @name = name 
+    @grade = grade
+  end 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
