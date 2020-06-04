5e330b60ab800cc47c76ba1a01fdeb727581f5c0
kiku ishimoto <kikuish@gmail.com>
2020-06-04 02:06:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,12 @@ class Student
 
   end
 
+  def self.drop_table 
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end 
+    
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
