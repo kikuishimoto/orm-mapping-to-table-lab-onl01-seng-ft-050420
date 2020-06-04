322f99e97b5a711639ea387ad288e4f28fc86618
kiku ishimoto <kikuish@gmail.com>
2020-06-04 02:10:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,8 +27,15 @@ class Student
     DB[:conn].execute(sql)
   end 
 
+  def self.save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES(?, ?)
+      SQL
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+    DB[:conn].execute(sql, self.name, self.album)
+  end 
+
+
+
 
 end
